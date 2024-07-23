52889924bb7a3f385bd30d5a29deb75b5f6ce596
rafli1127 <mmuhammadrafli.tkj@gmail.com>
2024-07-23 12:59:19 +0700

    update donasi dan modal dan fungsi

--- assets/JavaScript/raf.js
+++ assets/JavaScript/raf.js
@@ -1,13 +1,32 @@
 function showCards(keyword) {
-    var cards = document.querySelectorAll('.donasi-card');
-    
-    cards.forEach(function(card) {
-      var title = card.querySelector('.donasi-card-title').innerText.toLowerCase();
+  var cards = document.querySelectorAll('.donasi-card');
+  
+cards.forEach(function(card) {
+    var title = card.querySelector('.donasi-card-title').innerText.toLowerCase();
       
-      if (title.includes(keyword.toLowerCase())) {
+    if (title.includes(keyword.toLowerCase())) {
         card.style.display = 'block';
-      } else {
-        card.style.display = 'none'; 
-      }
-    });
-  }
+    } else {
+        card.style.display = 'none';
+    }
+  });
+}
+
+function changeButtonBackground() {
+  var buttons = document.querySelectorAll('.donasi-list-button');
+
+  buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+          buttons.forEach(function(btn) {
+              btn.style.backgroundColor = 'transparent';
+              btn.style.fontWeight = 'normal';
+          });
+          button.style.backgroundColor = '#E7C873';
+          button.style.fontWeight = '500';
+      });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  changeButtonBackground();
+});
